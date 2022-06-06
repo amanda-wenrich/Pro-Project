e5d3c3122893fb739067a08a47a1dbe72d2b944d
amanda-wenrich <104859963+amanda-wenrich@users.noreply.github.com>
2022-06-06 13:47:43 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,37 +1,34 @@
-let now = new Date();
-let today = document.querySelector("today");
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
-let date = now.getDate();
-let year = now.getFullYear();
-let hours = now.getHours();
-let minutes = now.getMinutes();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-today.innerHTML = `${day}, ${month} ${date}, ${year} @ ${hours}:${minutes} ET`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+}
+
+today.innerHTML = `${day}, ${date}, ${hours}:${minutes}`;
 
 function displayCity(event) {
   event.preventDefault();
