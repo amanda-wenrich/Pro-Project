b0dbaae058af2bb0e6285b96e76bbc46ced6c522
amanda-wenrich <104859963+amanda-wenrich@users.noreply.github.com>
2022-06-10 07:40:01 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -46,6 +46,14 @@ function displayCity(event) {
 
   axios.get(apiUrl).then(showWeather);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
@@ -59,7 +67,7 @@ function displayForecast(response) {
         forecastHTML +
         `
 <div class="col-2">
-  <div class="forecast-date">${forecastDay.dt}</div>
+  <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
   <img 
     src="http://openweathermap.org/img/wn/${
       forecastDay.weather[0].icon
